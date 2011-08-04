8b8c8d3e848d4a13547bc8c30d39faad6a26b60b
Ilya Grigorik <ilya@igvita.com>
2011-08-03 23:55:49 -0400

    switch to initializer for production support

--- lib/closure-sprockets/railtie.rb
+++ lib/closure-sprockets/railtie.rb
@@ -1,9 +1,9 @@
-module KlosureProcessor
+module ClosureProcessor
   class Railtie < Rails::Railtie
     config.closure = ActiveSupport::OrderedOptions.new
     config.closure.lib = 'vendor/assets/closure-library/closure'
 
-    config.after_initialize do |app|
+    initializer :setup_closure do |app|
       app.assets.append_path config.closure.lib
       app.assets.register_preprocessor 'application/javascript', ClosureDependenciesProcessor
     end
