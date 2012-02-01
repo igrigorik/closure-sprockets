27470cc89621839fe589078599a6107295b16310
Egor Khmelev <hmelyoff@gmail.com>
2012-02-01 22:20:11 +0400

    proper deps calculation based on closure python script

--- lib/closure-sprockets/directive_processor.rb
+++ lib/closure-sprockets/directive_processor.rb
@@ -1,22 +1,21 @@
-class ClosureDependenciesProcessor < Tilt::Template
-  def prepare; end
-
-  def evaluate(context, locals, &block)
-    context.require_asset 'goog/base'
-    context.require_asset 'soyutils'
-
-    data.lines.each do |line|
-
-      if line =~ /goog\.require\s*\(\s*[\'\"]([^\)]+)[\'\"]\s*\)/
-        goog, mod, sub = $1.split(".")
-        next if mod =~ /^Test/
-        sub = mod if sub.nil?
-
-        dep = [goog, mod, sub].compact.join("/")
-        context.require_asset(dep)
-      end
+class ClosureDirectiveProcessor < Sprockets::DirectiveProcessor
+  def process_require_closure_directive(path)
+    # Locate python script that helps us to calculate deps
+    cmd = File.expand_path "../bin/build/closurebuilder.py", File.dirname(__FILE__)
+    
+    # Get only application lookup paths
+    context.environment.paths.select{ |p| p if p.include? context.environment.root }.each do |path|
+      cmd << " --root=#{path}"
     end
 
-    data
+    # closurebuilder.py needs some input files to start deps searching
+    file_pathname = context.resolve path
+    cmd << " --input #{file_pathname}"
+    
+    # Read stdout result and split it by strings
+    results = `#{cmd}`.split "\n"
+    
+    # Finally require calculated deps
+    results.each{|dep| context.require_asset dep };
   end
 end
--- lib/closure-sprockets/railtie.rb
+++ lib/closure-sprockets/railtie.rb
@@ -1,13 +1,11 @@
 module ClosureProcessor
   class Railtie < Rails::Engine
-    config.closure = ActiveSupport::OrderedOptions.new
-    config.closure.lib = 'vendor/assets/closure-library/closure'
-
     initializer :setup_closure do |app|
-      app.assets.append_path config.closure.lib
-      app.assets.append_path 'vendor/assets'
-
-      app.assets.register_preprocessor 'application/javascript', ClosureDependenciesProcessor
+      app.assets.cache = false
+      
+      app.assets.unregister_preprocessor 'application/javascript', Sprockets::DirectiveProcessor
+      app.assets.register_preprocessor 'application/javascript', ClosureDirectiveProcessor
+      
       app.assets.register_engine '.soy', SoyTemplateProcessor
       app.assets.register_engine '.gss', GssTemplateProcessor
     end
