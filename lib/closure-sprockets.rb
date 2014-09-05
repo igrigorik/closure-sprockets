ad88276eb3512027d4b8d326117fabdd44fd69d8
Postmodern <postmodern.mod3@gmail.com>
2014-09-04 17:47:34 -0700

    closure-sprockets/*.rb -> closure/sprockets/*.rb

--- lib/closure-sprockets.rb
+++ lib/closure-sprockets.rb
@@ -1,7 +1,7 @@
 require "tilt"
 
-require "closure-sprockets/version"
-require "closure-sprockets/directive_processor"
-require "closure-sprockets/soy_processor"
-require "closure-sprockets/gss_processor"
-require "closure-sprockets/railtie" if defined?(Rails)
+require "closure/sprockets/version"
+require "closure/sprockets/directive_processor"
+require "closure/sprockets/soy_processor"
+require "closure/sprockets/gss_processor"
+require "closure/sprockets/railtie" if defined?(Rails)
